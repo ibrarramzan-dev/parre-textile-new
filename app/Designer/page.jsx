1295f0ca11432bd84efbec44f43b6c5d88a518a7
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-17 02:00:20 +0500

    asf

--- app/Designer/page.jsx
+++ app/Designer/page.jsx
@@ -8,38 +8,43 @@ export default function Home() {
   return (
     <>
       <div className={style.number}>
+        <h2>
+          Designer Quotes
+        </h2>
         <div className={style.allquotes}>
           <Quote />
         </div>
       </div>
     </>
-  )
+  );
 }
 
 const Quote = () => {
   return (
     <>
-      {
-        quotes.map((quote, index) => (
-          <div key={index}>
-            <div className={style.para}>
-              <div className={style.logo}><FaQuoteLeft className={style.logo} /></div>
-              <div className={style.quote}>
-                {quote.quote}
+      {quotes.map((quote, index) => (
+        <div key={index}>
+          <div className={style.para}>
+            <div className={style.logo}>
+              <FaQuoteLeft className={style.logo} />
+            </div>
+            <div className={style.quote}>{quote.quote}</div>
+            <div className={style.author}>
+              <div className={style.avatar}>
+                {quote.avatarImg === "" ? (
+                  <RxAvatar />
+                ) : (
+                  <Image src={quote.avatarImg} alt="" />
+                )}
               </div>
-              <div className={style.author}>
-                <div className={style.avatar}>
-                  {quote.avatarImg === "" ? <RxAvatar /> : <Image src={quote.avatarImg} alt="" />}
-                </div>
-                <div className={style.name}>
-                  <span className={style.author}>{quote.author}</span>
-                  <span className={style.expertise}>{quote.expertise}</span>
-                </div>
+              <div className={style.name}>
+                <span className={style.author}>{quote.author}</span>
+                <span className={style.expertise}>{quote.expertise}</span>
               </div>
             </div>
           </div>
-        ))
-      }
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
