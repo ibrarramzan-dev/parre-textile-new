99d3b9aba0d17f67ee300920140d1e5800d7a72d
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-22 07:32:39 +0500

    push is open

--- Components/quotes/quotesArray.js
+++ Components/quotes/quotesArray.js
@@ -1,22 +1,35 @@
 export const quotes = [
   {
     quote:
-      "Color is a power which directly influences the soul",
-    author: "Wassily Kadinsky",
-    expertise: "Russian Painter",
+    "The essence of interior design will always be about people and how they live",
+    author: "Albert Hadley",
+    expertise: "Interior Designer",
     avatarImg: "",
   },
   {
     quote:
-      "The essence of interior design will always be about people and how they live",
-    author: "Albert Hadley",
+    "Always put something mad on top of something very good, or something very good on top of something mad",
+    author: "Geoffrey Bennison",
     expertise: "Interior Designer",
     avatarImg: "",
   },
   {
     quote: "Tradional interior design is a timeless, elegant look that never goes out of style",
     author: "Mary McDonald",
-    expertise: "Actor",
+    expertise: "Interior Designer",
+    avatarImg: "",
+  },
+  {
+    quote: "You can get rid of your husband, but never get rid of your curtain maker",
+    author: "Nina Campbell",
+    expertise: "Interior Designer",
+    avatarImg: "",
+  },
+  {
+    quote:
+      "Color is a power which directly influences the soul",
+    author: "Wassily Kadinsky",
+    expertise: "Russian Painter",
     avatarImg: "",
   },
 ];
