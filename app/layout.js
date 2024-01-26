8222b4257710075f9f4fa16a93727ce576ba1df4
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-26 14:39:27 +0500

    done

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,51 @@
 import "@/styles/App.scss";
-import Head from "next/head";
-import favicon1 from "@/public/favicon-16x16.png";
-import favicon2 from "@/public/favicon-32x32.png";
-import favicon4 from "@/public/android-chrome-512x512.png";
-import apple from "@/public/apple-touch-icon.png";
+// import Head from "next/head";
+// import favicon1 from "@/public/favicon-16x16.png";
+// import favicon2 from "@/public/favicon-32x32.png";
+// import favicon3 from "@/public/android-chrome-192x192.png";
+// import favicon4 from "@/public/android-chrome-512x512.png";
 
 export const metadata = {
   title: "PARRE' Textile",
   description:
     "Luxury Printed Textiles Exclusively for Discerning Interior Designers",
+    icons:{
+      icon:['/favicon.ico'],
+      apple:['/apple-touch-icon.png?v=4'],
+      shortcut:['/apple-touch-icon.png']
+    }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" type="image/png" sizes="16x16" href={favicon1} />
-        <link rel="icon" type="image/png" sizes="32x32" href={favicon2} />
-        <link rel="icon" type="image/png" sizes="512x512" href={favicon4} />
-        <link rel="apple-touch-icon" sizes="180x180" href={apple} />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <body>{children}</body>
+      {/* <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={favicon1}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={favicon2}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={favicon3}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href={favicon4}
+        />
+      </Head> */}
+      <body suppressContentEditableWarning>{children}</body>
     </html>
   );
 }
