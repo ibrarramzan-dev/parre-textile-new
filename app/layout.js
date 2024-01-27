a56a5c89932a90a03293fff5904ede9194e75842
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-28 01:29:01 +0500

    done

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,4 @@
 import "@/styles/App.scss";
-// import Head from "next/head";
-// import favicon1 from "@/public/favicon-16x16.png";
-// import favicon2 from "@/public/favicon-32x32.png";
-// import favicon3 from "@/public/android-chrome-192x192.png";
-// import favicon4 from "@/public/android-chrome-512x512.png";
 
 export const metadata = {
   title: "PARRE' Textile",
@@ -19,32 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={favicon1}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={favicon2}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href={favicon3}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href={favicon4}
-        />
-      </Head> */}
       <body suppressContentEditableWarning>{children}</body>
     </html>
   );
